bfae4577a3846ebc50b5c6ebb4b0a3e679b3d09b
Nguyen Van Nam <boychauphong2001@gmail.com>
2021-08-22 13:04:59 +0700

    done init data

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Column from '../Column/Column'
 import './Dashboard.scss'
+import { initData } from '../../actions/initData'
+import { mapOrder } from '../../util/sort'
 const DashBoard = () => {
+    const [columnState, setColumnState] = useState([])
+    const [boardState, setBoardState] = useState({})
+    useEffect(() => {
+        const boardFromDb = initData.boards.find((board) => {
+            return board.id === 'board-1'
+        })
+        setBoardState(boardFromDb)
+        setColumnState(
+            mapOrder(boardFromDb.columns, boardFromDb.columnOrder, 'id')
+        )
+    }, [])
     return (
         <div className="dashboard">
+            {!columnState ? (
+                <h1>No column here</h1>
+            ) : (
+                columnState.map((column, idx) => (
+                    <Column key={idx} column={column}></Column>
+                ))
+            )}
+            {/* <Column></Column>
             <Column></Column>
             <Column></Column>
-            <Column></Column>
             <div className="column">
                 <h3 className="header">Mission 1</h3>
                 <ul>
@@ -55,6 +75,7 @@ const DashBoard = () => {
                     <button>Add new task</button>
                 </div>
             </div>
+         */}
         </div>
     )
 }
