5173596afe5dd1397fbee269341d3a8322e8a1f2
Nguyen Van Nam <boychauphong2001@gmail.com>
2021-09-01 23:22:58 +0700

    combine_API_CRUD

--- src/actions/apiCall/index.js
+++ src/actions/apiCall/index.js
@@ -5,3 +5,27 @@ export const fetchBoard = async (id) => {
     console.log(res)
     return res.data
 }
+export const createNewTask = async (data) => {
+    const res = await axios.post(`${API_ROOT}/task`, data)
+    console.log(res)
+    return res.data.data
+}
+export const updateTask = async (data) => {
+    const res = await axios.put(`${API_ROOT}/task/${data._id}`, data)
+    console.log(res)
+    return res.data.data
+}
+export const createNewColumn = async (data) => {
+    const res = await axios.post(`${API_ROOT}/column`, data)
+    console.log(res)
+    return res.data.data
+}
+export const updateColumn = async (id, data) => {
+    const res = await axios.put(`${API_ROOT}/column/${id}`, data)
+
+    return res.data.data
+}
+export const deleteColumn = async (id) => {
+    const res = await axios.delete(`${API_ROOT}/column/${id}`)
+    return res.data.data
+}
